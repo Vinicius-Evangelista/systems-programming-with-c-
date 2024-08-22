868b7cf892196b25cbf92415c0fe9f6b108119d6
Vinicius Evangelista <vini.evangelista@live.com>
2024-08-22 20:52:40 -0300

    feat: add WriteLine using Windows Libraries

---
+++ PInvoke/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+static extern bool WriteConsole
+(IntPtr hConsoleOutput, 
+ string lpBuffer,
+ uint nNumberOfCharsToWrite,
+ out uint lpNumbersOfCharsWritten,
+ IntPtr lpReserved
+);
+
+[DllImport("Kernel32.dll", SetLastError = true)]
+static extern IntPtr GetStdHandle(int nStdHandle);
+
+const int STD_OUTPUT_HANDLE = -11;
+
+IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+if (stdHandle == IntPtr.Zero)
+{
+    Console.WriteLine("Could not retrieve standard output");
+    return;
+}
+
+string output = "Hello, System Programmers!";
+
+uint charsWritten;
+
+if (!WriteConsole(stdHandle, output, (uint)output.Length, out charsWritten, IntPtr.Zero))
+{
+    Console.WriteLine("Failed to write using Win32 API.");
+}
+
+
